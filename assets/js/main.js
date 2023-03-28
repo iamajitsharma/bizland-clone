8c693e4db9b8386c0ff508aef9bd91941f1fad29
Ajit Sharma <ajitsharma2021@gmail.com>
2023-03-28 20:08:55 +0530

    Almost Completed

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict";
+
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1200,
+      easing: "ease-in-out",
+      once: "true",
+      mirror: false,
+    });
+  });
+})();
